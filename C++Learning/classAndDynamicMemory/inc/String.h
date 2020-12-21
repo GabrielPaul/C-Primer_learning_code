1d7ab5efe676a4bd8c594e43403f8b9946e4da0e
Gabriel <benguakeji@liutao.com>
2020-12-21 18:08:24 +0800

    modify class String methods.

--- C++Learning/classAndDynamicMemory/inc/String.h
+++ C++Learning/classAndDynamicMemory/inc/String.h
@@ -5,26 +5,26 @@ private:
 	char* str;	//pointer to string
 	int len;	//length of string
 	static int num_strings;	//number if objects
+	static const int CINLIM = 80;
 
 public:
 	String(const char* s);
 	String();
-	~String();
 	String(const String& st);	//copy constructor
-	String& operator =(const String& st);
+	~String();
 
-	//friend function
-	friend std::ostream& operator <<(std::ostream& os,
-		const String& st);
-
-	//add new functions
 	int length()const { return len; };
+	//overloaded operator methods
+	String& operator =(const String& st);
+	String& operator =(const char* s);
+	char& operator[](int i);
+	const char& operator[](int i)const;
+	//overload operator friends
 	friend bool operator<(const String& st, const String& st2);
 	friend bool operator>(const String& st, const String& st2);
 	friend bool operator==(const String& st, const String& st2);
+	friend std::ostream& operator <<(std::ostream& os,const String& st);
 	friend std::istream& operator>>(std::istream& is,String& st);
-	char& operator[](int i);
-	const char& operator[](int i)const;
+	//static function
 	static int HowMany();
-	String& operator =(const char* s);
 };
--- C++Learning/classAndDynamicMemory/src/String.cpp
+++ C++Learning/classAndDynamicMemory/src/String.cpp
@@ -18,18 +18,18 @@ String::String()
 	num_strings++;
 }
 
-String::~String()
-{
-	--num_strings;
-	delete[]str;
-}
-
 String::String(const String& st)
 {
 	num_strings++;
 	len = st.len;
 	str = new char[len + 1];
 	std::strcpy(str, st.str);
+}
+
+String::~String()
+{
+	--num_strings;
+	delete[]str;
 }
 
 String& String::operator=(const String& st)
@@ -45,10 +45,25 @@ String& String::operator=(const String& st)
 	return *this;
 }
 
-std::ostream& operator<<(std::ostream& os, const String& st)
+String& String::operator=(const char* s)
 {
-	os << st.str;
-	return os;
+	delete[] str;
+	len = std::strlen(s);
+	str = new char[len + 1];
+	std::strcpy(str, s);
+	return *this;
+}
+
+//for use with const String objects
+const char& String::operator[](int i) const
+{
+	return str[i];
+}
+
+char& String::operator[](int i)	//declare as char&,can be used 'String means("test");
+								// means[0] = 'r'; '
+{
+	return str[i];
 }
 
 //The strcmp function is in alphabetical order, 
@@ -63,42 +78,39 @@ bool operator<(const String& st, const String& st2)
 
 bool operator>(const String& st, const String& st2)
 {
-	return st2.str<st.str;
+	return st2.str < st.str;
 }
 
 bool operator==(const String& st, const String& st2)
 {
-	return (std::strcmp(st.str,st2.str)==0);
+	return (std::strcmp(st.str, st2.str) == 0);
+}
+
+std::ostream& operator<<(std::ostream& os, const String& st)
+{
+	os << st.str;
+	return os;
 }
 
 std::istream& operator>>(std::istream& is, String& st)
 {
-	is >> st;
+	char temp[String::CINLIM];
+	is.get(temp, String::CINLIM);
+	if (is) //If for some reason (if the end of the file is reached, 
+			//or get(char*,int) reads a blank line), the input fails, 
+			//the value of the istream object will be set to false.
+	{
+		st = temp;
+	}
+	while (is && is.get() != '\n') 
+	{
+		continue;
+	}
 	return is;
 }
 
-char& String::operator[](int i)	//declare as char&,can be used 'String means("test");
-								// means[0] = 'r'; '
-{
-	return str[i];
-}
-
-//for use with const String objects
-const char& String::operator[](int i) const
-{
-	return str[i];
-}
-
+//static method
 int String::HowMany()
 {
 	return num_strings;
-}
-
-String& String::operator=(const char* s)
-{
-	delete[] str;
-	len = std::strlen(s);
-	str = new char[len+1];
-	std::strcpy(str,s);
-	return *this;
 }
