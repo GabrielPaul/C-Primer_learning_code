05d291e89037a1f16c26c822142347c67f91c475
Gabriel <benguakeji@liutao.com>
2020-12-21 16:44:44 +0800

    fix new memory error

--- C++Learning/classAndDynamicMemory/src/strngbad.cpp
+++ C++Learning/classAndDynamicMemory/src/strngbad.cpp
@@ -12,7 +12,7 @@ int StringBad::num_strings = 0;
 StringBad::StringBad(const char* s) 
 {
 	len = std::strlen(s);		//set size
-	str = new char(len + 1);	//allot storage
+	str = new char[len + 1];	//allot storage
 	std::strcpy(str, s);		//set object count
 	num_strings++;
 	cout << num_strings << ": \"" << str 
